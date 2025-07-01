15a909f4251cee932fc5f0c2edfbc5aac3a19eb0
USUARIO <USUARIO@MacBook-Air.local>
2025-07-01 12:22:36 -0300

    Clase 4 Backend

--- tests/specs/auth.spec.ts
+++ tests/specs/auth.spec.ts
@@ -39,7 +39,7 @@ test.describe('Grupo 2: Casos Negativos (Validaciones y Errores)', () => {
         await loginPage.getInvlidCredentialsMessage();
     });
 
-   test('Test 2.2: Intento de Login con Campos Vacíos', async ({ page }) => {
+    test('Test 2.2: Intento de Login con Campos Vacíos', async ({ page }) => {
         await loginPage.completeLogin('', '');
         await expect(page).toHaveURL(pages.login);
         const validationMessage = await loginPage.getEmailValidationMessage();
@@ -62,7 +62,7 @@ test.describe('Grupo 2: Casos Negativos (Validaciones y Errores)', () => {
 
 });
 
-    test.describe('Grupo 3: Flujo de Sesión y Navegación', () => {
+test.describe('Grupo 3: Flujo de Sesión y Navegación', () => {
     let loginPage: LoginPage;
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page);
@@ -79,5 +79,27 @@ test.describe('Grupo 2: Casos Negativos (Validaciones y Errores)', () => {
         await dashboardPage.getCerrarSesion();
         await page.goto(pages.dashboard);
         await expect(page).toHaveURL(pages.login);
+    });
+});
+
+test.describe('Grupo 4: Pruebas de API', () => {
+    test('Test 4.1: Verificación de Respuesta Exitosa de la API de Login', async ({ page, request }) => {
+        const response = await request.post('http://localhost:4000/api/auth/login', {
+            data: {
+                email: Register.email,
+                password: Register.password
+            }
+        });
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('token');
+        expect(typeof responseBody.token).toBe('string');
+        expect(responseBody).toHaveProperty('user');
+        expect(responseBody.user).toEqual(expect.objectContaining({
+            id: expect.any(String),
+            firstName: Register.firstName,
+            lastName: Register.lastname,
+            email: Register.email
+        }));
     });
 });
